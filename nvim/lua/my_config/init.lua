2177f34f6105e907729681a9bb2cfcd5d43fc424
Maksim Bakhanovich <maksim.bakhanovich@proton.me>
2024-03-09 19:49:54 +0300

    chore: update nvim config via kickstart.nvim

--- nvim/lua/my_config/init.lua
+++
@@ -1,9 +0,0 @@
-require("my_config.remap")
-require("my_config.set")
-require("onedark").setup {
-    style = "warmer",
-    transparent = false,
-    term_colors = false,
-}
-require("onedark").load()
-vim.cmd.colorscheme("onedark")
